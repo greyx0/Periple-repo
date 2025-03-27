f1a4f8902eec630c795f826fe7922c167504dbdf
greyx0 <119888119+greyx0@users.noreply.github.com>
2025-03-28 00:29:17 +0100

    player base

---
+++ périple/Player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@onready var screen_size = get_viewport_rect().size
+@export var speed = 300.0
+
+const standard_bullet = preload("res://Bullets/standard/standard_bullet.tscn")
+
+func _ready():
+	pass
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+
+	get_input()
+	move_and_slide()
+	screen_wrap()
+
+func screen_wrap():
+	position.y = wrapf(position.y, 0, screen_size.y)
